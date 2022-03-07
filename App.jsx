57fe1a2f9fb0f27f1f4563e4743335790221a1d3
a-yazy <a-yazy@users.noreply.github.com>
2022-03-07 14:44:48 +0900

    Create Hello component

--- App.jsx
+++ App.jsx
@@ -1,10 +1,27 @@
+/*
+  *** 実行方法 ***
+  @PowerShell-1
+  #エミュレータ一覧表示
+  emulator -list-avds
+
+  #エミュレータ起動（with ネット）
+  emulator -avd Pixel_5_API_30 -dns-server 8.8.8.8
+
+  @PowerShell-2
+  #expo起動（ワークディレクトリで）
+  expo start
+*/
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Hello from './src/components/Hello';
+
 export default function App() {
   return (
     <View style={styles.container}>
+      <Hello bang>World</Hello>
+      <Hello style={{ fontSize: 16 }}>Small World</Hello>
       <Text>Open up App.js to start working on your app!</Text>
       {/* eslint-disable-next-line */}
       <StatusBar style="auto" />
