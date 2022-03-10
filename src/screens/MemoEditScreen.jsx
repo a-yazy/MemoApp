e40acfef70282f9d02085e687418907294244ef9
a-yazy <a-yazy@users.noreply.github.com>
2022-03-10 17:13:32 +0900

    Update memo data on MemoEditScreen

--- src/screens/MemoEditScreen.jsx
+++ src/screens/MemoEditScreen.jsx
@@ -1,24 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  StyleSheet, View, TextInput, KeyboardAvoidingView,
+  StyleSheet, View, TextInput, KeyboardAvoidingView, Alert,
 } from 'react-native';
+import { string, shape } from 'prop-types';
+import firebase from 'firebase';
 
 import CircleButton from '../components/CircleButton';
 
 export default function MemoEditScreen(props) {
-  const { navigation } = props;
+  const { navigation, route } = props;
+
+  // 遷移元からのパラメータ
+  const { id, bodyText } = route.params;
+  const [body, setBody] = useState(bodyText);
+
+  // --------------------
+  // Checkボタン押下時の処理
+  // --------------------
+  const handlePress = () => {
+    // ログインユーザー取得
+    const { currentUser } = firebase.auth();
+
+    // ログインユーザーが取得できる（ログイン状態）場合
+    if (currentUser) {
+      // firestore取得
+      const db = firebase.firestore();
+      // collection(ユーザーごとmemos)への参照を取得
+      const ref = db.collection(`users/${currentUser.uid}/memos`).doc(id);
+      // ドキュメント更新（キー＝"bodyText"、値＝bodyText）
+      ref.set({
+        bodyText: body,
+        updatedAd: new Date(),
+      }, { marge: true })
+        .then(() => {
+          // 画面遷移（遷移元に戻る）
+          navigation.goBack();
+        })
+        .catch((error) => {
+          // ドキュメント更新ＮＧ
+          Alert.alert(error.code);
+        });
+    }
+  };
+
   return (
     <KeyboardAvoidingView style={styles.container} behavior="null">
       <View style={styles.inputContainer}>
-        <TextInput value="買い物リスト" multiline style={styles.input} />
+        <TextInput
+          multiline
+          value={body}
+          onChangeText={(text) => { setBody(text); }}
+          style={styles.input}
+          autoFocus
+        />
       </View>
       <CircleButton
         name="check"
-        onPress={() => { navigation.goBack(); }}
+        onPress={handlePress}
       />
     </KeyboardAvoidingView>
   );
 }
+
+MemoEditScreen.propTypes = {
+  route: shape({
+    params: shape({ id: string, bodyText: string }),
+  }).isRequired,
+};
 
 const styles = StyleSheet.create({
   container: {
